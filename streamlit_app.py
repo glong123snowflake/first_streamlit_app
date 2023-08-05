27a7b8df73a16714253cb4d729cc6c2948368845
glong123snowflake <141530535+glong123snowflake@users.noreply.github.com>
2023-08-05 16:53:55 -0700

    Update streamlit_app.py
    added smoothies option

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 streamlit.header('Breakfast Favorites')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
