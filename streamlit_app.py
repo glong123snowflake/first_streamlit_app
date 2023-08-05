4df743142bcefd0e145fc9ad9d646fb1c328e305
glong123snowflake <141530535+glong123snowflake@users.noreply.github.com>
2023-08-05 16:54:51 -0700

    Update streamlit_app.py
    moved the smoothies to the bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 streamlit.header('Breakfast Favorites')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free-range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
